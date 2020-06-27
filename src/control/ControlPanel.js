ab1093889ec0e43d1203af6be8a820757c34e1ec
kobaeugenea <kobaeugenea@gmail.com>
2020-06-27 17:37:55 +1000

    Toolbar

--- src/control/ControlPanel.js
+++ src/control/ControlPanel.js
@@ -3,6 +3,7 @@ import {APPLICATION_MODE} from '../enums/settings.js'
 import './ControlPanel.css';
 import NormalModePanel from "./NormalModePanel";
 import RoundModePanel from "./round/RoundModePanel";
+import Toolbar from "./Toolbar";
 
 export default class ControlPanel extends Component {
 
@@ -16,14 +17,6 @@ export default class ControlPanel extends Component {
             this.state.view = APPLICATION_MODE.ROUND;
         }
         return <div className='controlPanel'>
-            <input
-                className="btn btn-large btn-danger"
-                type="button"
-                id="buttonLeaveSession"
-                onClick={this.props.leaveSession}
-                value="Leave session"
-            />
-
             {this.props.applicationMode === APPLICATION_MODE.NORMAL &&
             <div className={this.state.view === APPLICATION_MODE.NORMAL ? 'changeToRound' : 'changeToNormal'}
                  onClick={() => {
@@ -42,6 +35,8 @@ export default class ControlPanel extends Component {
                                   speakingQueue={this.props.speakingQueue}
                                   userStream={this.props.userStream}
                                   roundTime={this.props.roundTime}/>}
+
+            <Toolbar userStream={this.props.userStream} leaveSession={this.props.leaveSession}/>
 
             <footer className="footer"/>
         </div>;
